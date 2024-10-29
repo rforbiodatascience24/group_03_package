e5b256106877d3773f07d7c94a6a91dbee814787
isgoldsch <ida2108@live.dk>
2024-10-29 09:39:03 +0100

    Added files

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Title
+#'
+#' Description
+#' It is a table of translations of codons
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_table"
